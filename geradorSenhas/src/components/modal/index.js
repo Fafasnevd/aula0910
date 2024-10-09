fa2404ecc8cfd1dc9af382a3be3b27760b64ee53
Fafasnevd <rafael.sartori.etec@gmail.com>
2024-10-09 17:54:32 +0000

    AA

--- geradorSenhas/src/components/modal/index.js
+++ geradorSenhas/src/components/modal/index.js
@@ -1,12 +1,26 @@
 import { View, Text, StyleSheet, TouchableOpacity, Pressable } from "react-native";
+import * as Clipboard from 'expo-clipboard';
+import Toast from 'react-native-toast-message';
 
 export function ModalPassword({senha, handleClose}){
+
+  function copyToClipboard() {
+    Clipboard.setStringAsync(senha);
+    Toast.show({
+      type: 'success',
+      text1: 'Senha copiada!',
+      text2: 'A senha foi copiada para a área de trasnferência 👍',
+    });
+   
+  }
+
+
   return(
     <View style={styles.container}>
       <View style={styles.content}>
         <Text style={styles.title}>Senha gerada</Text>
 
-        <Pressable style={styles.innerPassword}>
+        <Pressable style={styles.innerPassword} onPress={copyToClipboard}>
           <Text style={styles.text}>
           {senha}
           </Text>
@@ -23,6 +37,8 @@ export function ModalPassword({senha, handleClose}){
         </View>
 
       </View>
+
+      <Toast/>
     </View>
   )
 }
